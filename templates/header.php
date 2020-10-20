3f5acd6854da7b3be155fdbe52f238c3325270a8
Toasty3107 <59958494+Toasty3107@users.noreply.github.com>
2020-10-20 10:13:51 +0200

    .

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,20 @@
 <header class="jumbotron">
-        <h1>Festlplaner</h1>
+
+  <ul class="nav nav-pills nav-fill">
+  <li class="nav-item">
+    <a class="nav-link" href="#">Festlplanner</a>
+  </li>
+      <div style="margin: 0 auto;">
+  <li class="nav-item">
+      <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+
+  </li>
+           </div>
+  <li class="nav-item">
+    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Profil</a>
+  </li>
+</ul>
 </header>
