92ed21afaabc9bb3d17fe9818da7a45516099b22
Toasty3107 <59958494+Toasty3107@users.noreply.github.com>
2020-10-20 10:40:47 +0200

    .

--- templates/header.php
+++ templates/header.php
@@ -1,20 +1,21 @@
 <header class="jumbotron">
 
-  <ul class="nav nav-pills nav-fill">
+<ul class="nav nav-pills nav-fill">
   <li class="nav-item">
-    <a class="nav-link" href="#">Festlplanner</a>
-  </li>
-      <div style="margin: 0 auto;">
-  <li class="nav-item">
+      <h1 style="color:white">Festlplaner</h1>
+</li>
+
+<div style="margin: 0 auto;">
+<li class="nav-item">
       <form class="form-inline">
     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+    </form>
+</li>
+</div>
 
-  </li>
-           </div>
   <li class="nav-item">
-    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Profil</a>
-  </li>
+    <a class="nav-link" href="profil.php" aria-disabled="true">Profil</a>
+</li>
 </ul>
 </header>
