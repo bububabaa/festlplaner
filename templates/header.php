0c71e4dc37a39cad752192f0cd3f35a96c60b4e4
Toasty3107 <59958494+Toasty3107@users.noreply.github.com>
2020-10-22 10:39:45 +0200

    .

--- templates/header.php
+++ templates/header.php
@@ -23,10 +23,10 @@
 <div style="margin: 0 auto;">
 <li class="nav-item">
       <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
           <link rel="stylesheet" href="assets/img/search_icon.png">
+          <button class="my-2 my-sm-0" class="search-icon"> </button>
     </form>
-    <button class="my-2 my-sm-0" class="search-icon"> </button>
 
 </li>
 </div>
