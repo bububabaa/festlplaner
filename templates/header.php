37ccdc0af4d3d5570a13e27f0f6f46bb9792d8d4
bububabaa <59958434+bububabaa@users.noreply.github.com>
2020-11-03 10:45:50 +0100

    .

--- templates/header.php
+++ templates/header.php
@@ -29,16 +29,22 @@
 
         }
 
-        .sidenav a {
-            padding: 8px 8px 8px 32px;
-            text-decoration: none;
-            font-size: 25px;
-            color: #818181;
-            display: block;
-            transition: 0.3s;
-        }
+        .sidenav a, .dropdown-btn {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  border: none;
+  background: none;
+  width: 100%;
+  text-align: left;
+  cursor: pointer;
+  outline: none;
+  transition: 0.3s;
+}
 
-        .sidenav a:hover {
+        .sidenav a:hover, .dropdown-btn:hover {
             color: #f1f1f1;
         }
 
@@ -50,6 +56,27 @@
             margin-left: 50px;
         }
 
+.active {
+  background-color: grey;
+  color: white;
+}
+
+        .dropdown-container {
+  display: none;
+  padding-left: 8px;
+}
+
+/* Optional: Style the caret down icon */
+.fa-caret-down {
+  float: right;
+  padding-right: 8px;
+}
+
+/* Some media queries for responsiveness */
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
     </style>
 
     <ul class="nav nav-pills nav-fill">
@@ -75,10 +102,18 @@
 
     <div id="mySidenav" class="sidenav">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Clients</a>
-        <a href="#">Contact</a>
+        <a href="#">Home</a>
+        <a href="#">Kalender</a>
+        <a href="#">Galerie</a>
+        <a href="#">Karte</a>
+        <button class="dropdown-btn">Abos
+    <i class="fa fa-caret-down"></i>
+  </button>
+  <div class="dropdown-container">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
     </div>
 
     <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
@@ -91,8 +126,48 @@
         function closeNav() {
             document.getElementById("mySidenav").style.width = "0";
         }
+    </script>
+
+    <script>
+        var dropdown = document.getElementsByClassName("dropdown-btn");
+
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
 
     </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+<script>
+    const options = {
+  bottom: '64px', // default: '32px'
+  left: 'unset', // default: '32px'
+  right: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+  function addDarkmodeWidget() {
+    const darkmode = new Darkmode(options);
+      darkmode.showWidget();
+  }
+  window.addEventListener('load', addDarkmodeWidget);
+</script>
 
     <!-- Use any element to open the sidenav -->
 
