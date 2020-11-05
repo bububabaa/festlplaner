3645e8a1fc3825a0d4afdf527d7c1cc78989012d
Mehmet <mehmetaliderbabo@gmail.com>
2020-11-05 17:17:33 +0100

    .

---
+++ kalender.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<style>
+</style>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+
+    <title>Festlplaner</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!--
+Ramayana CSS Template
+https://templatemo.com/tm-529-ramayana
+-->
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-style.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+
+</head>
+
+<body class="is-preload">
+
+    <!-- Wrapper -->
+    <div id="wrapper">
+
+        <!-- Main -->
+        <div id="main">
+            <div class="inner">
+
+                <!-- Header -->
+                <header id="header">
+                    <div class="logo">
+                        <a href="index.html">Festlplaner</a>
+                    </div>
+                </header>
+      <!-- Sidebar -->
+        <div id="sidebar">
+
+            <div class="inner">
+
+                <!-- Search Box -->
+                <section id="search" class="alt">
+                    <form method="get" action="#">
+                        <input type="text" name="search" id="search" placeholder="Search..." />
+                    </form>
+                </section>
+
+                <!-- Menu -->
+                <nav id="menu">
+                    <ul>
+                        <li><a href="index.html">Homepage</a></li>
+                        <li><a href="simple_page.html">Simple Page</a></li>
+                        <li><a href="shortcodes.html">Shortcodes</a></li>
+                        <li><a href="kalender.php">Kalender</a></li>
+                        <li><a href="galerie.php">Galerie</a></li>
+                        <li><a href="karte.php">Karte</a></li>
+                        <li>
+                            <span class="opener">Abos</span>
+                            <ul>
+                                <li><a href="#">Abo 1</a></li>
+                                <li><a href="#">Abo 2</a></li>
+                                <li><a href="#">Abo 3</a></li>
+                            </ul>
+
+                        <li><a href="https://www.google.com">External Link</a></li>
+                    </ul>
+                </nav>
+
+                <script>
+                </script>
+</body>
+</html>
